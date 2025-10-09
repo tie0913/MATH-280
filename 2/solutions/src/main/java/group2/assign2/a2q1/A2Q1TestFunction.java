3ce959c5b09d539c33dba00b54aff14eb207a8e1
Tie Wang <wang9431@saskpolytech.ca>
2025-10-09 17:11:35 -0600

    q1 and q2 are done

--- 2/solutions/src/main/java/group2/assign2/a2q1/A2Q1TestFunction.java
+++ 2/solutions/src/main/java/group2/assign2/a2q1/A2Q1TestFunction.java
@@ -21,14 +21,19 @@ public class A2Q1TestFunction
         A2Q1IFunction xto2 = new Xto2();
         System.out.println("for question a = " + xto2.integrateLeftSimple(0, 10, 0.000001, 500));
 
-
         A2Q1IFunction xtox = new XtoX();
         System.out.println("for question b = " + xtox.integrateLeftSimple(1.5, 2.5, 0.000001, 500));
-
 
         A2Q1IFunction log2 = new Log2();
         System.out.println("for question c = " + log2.integrateLeftSimple(1, 9, 0.000001, 500));
 
         System.out.println("for question d = " + log2.integrateLeftSimple(2, 256, 0.000001, 500));
+
+
+        System.out.println(log2.calculate(2));
+        System.out.println(log2.calculate(4));
+        System.out.println(log2.calculate(6));
+        System.out.println(log2.calculate(8));
+        
     }
 }
